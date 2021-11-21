b1fa344fcd8ddd819be7189ab09a52dc4e98ae66
gcr07 <63270579+gcr07@users.noreply.github.com>
2021-11-21 14:53:55 -0600

    Create Helloworld.sol

---
+++ Helloworld.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.3 and less than 0.9.0
+pragma solidity ^0.8.3;
+
+contract HelloWorld {
+    string public greet = "Hello World!";
+    string  masa= " No se puede ver esta variable desde afuera";
+}
