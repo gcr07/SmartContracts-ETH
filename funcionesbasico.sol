c28a0bf92fa56cf4baee4f322209617dd0c103f4
gcr07 <63270579+gcr07@users.noreply.github.com>
2021-11-21 15:03:33 -0600

    Create funcionesbasico.sol

---
+++ funcionesbasico.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.3 and less than 0.9.0
+pragma solidity ^0.8.3;
+
+contract funciones {
+
+
+    uint public count;
+    string public variable= "HOla mundo ";
+
+    // Function to get the current count
+    function get() public view returns (uint) {
+        return count;
+    }
+
+    // Function to increment count by 1
+    function inc() public {
+        count += 1;
+    }
+
+    // Function to decrement count by 1
+    function dec() public {
+        count -= 1;
+    }
+    
+}
