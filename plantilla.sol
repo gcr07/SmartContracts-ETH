ad6bd7bae3d04cfdfa593fa92dec684533c0f896
gcr07 <63270579+gcr07@users.noreply.github.com>
2022-03-24 10:41:31 -0600

    Create plantilla.sol

---
+++ plantilla.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma  solidity ^0.8.6;// ^el simbolo nos dice que los ultimos numeros son lo que pueden cambiar los demas no
+
+contract Test1{ // NO IMPORTA QUE SE LLAME DE OTRA MANERA AL ARCHIVO
+    
+
+ 
+ 
+}
