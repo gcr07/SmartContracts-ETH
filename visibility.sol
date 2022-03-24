f22df957d0dac0c6708936fa59512f65439e46f5
gcr07 <63270579+gcr07@users.noreply.github.com>
2022-03-24 11:21:26 -0600

    Create visibility.sol

---
+++ visibility.sol
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+// https://solidity-by-example.org/visibility/
+
+/* Internal − Internal functions/ Variables can only be used
+ internally or by derived contracts. private
+  Private functions/ Variables can only be used internally
+    and not even by derived contracts.
+
+*/
