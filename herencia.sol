bf12fc262218c951cd07899c097d5c3d3d37f2ca
gcr07 <63270579+gcr07@users.noreply.github.com>
2022-03-25 09:44:31 -0600

    Update herencia.sol

--- herencia.sol
+++ herencia.sol
@@ -2,6 +2,31 @@
 
 //Single Inheritance
 
+// SPDX-License-Identifier: MIT
+
+pragma  solidity ^0.8.6;// ^el simbolo nos dice que los ultimos numeros son lo que pueden cambiar los demas no
+
+contract Base{
+
+uint256 var1 = 100; // variables de estado.
+
+function getVal() public view returns(uint256){
+return var1;
+
+}
+
+}
+
+contract Child is Base{
+
+function increment() public
+{
+var1 = var1 +1 ;
+}
+
+
+}
+
 
 /*
  El contrato del que otros contratos heredan características se conoce como contrato base, 
