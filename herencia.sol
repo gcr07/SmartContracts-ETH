6aff8a8fcfe03d81c550a37a076e54b1ef88024d
gcr07 <63270579+gcr07@users.noreply.github.com>
2022-03-24 22:36:58 -0600

    Create herencia.sol

---
+++ herencia.sol
@@ -0,0 +1,9 @@
+
+
+//Single Inheritance
+
+
+/*
+ El contrato del que otros contratos heredan características se conoce como contrato base, 
+ mientras que el contrato que hereda las características se denomina contrato derivado.
+*/
