a439834e38c826592d57e17986ebe9aee69c0108
gcr07 <63270579+gcr07@users.noreply.github.com>
2022-03-29 11:21:14 -0600

    Update Interacting with other contracts.sol

--- Interacting with other contracts.sol
+++ Interacting with other contracts.sol
@@ -17,3 +17,22 @@ contract LuckyNumber {
 contract NumberInterface { // Importante se crea un contrato nuevo
   function getNum(address _myAddress) public view returns (uint);// no tiene llaves ni nada
 }
+
+
+// PARA USARLO 
+
+contract MyContract {
+  address NumberInterfaceAddress = 0xab38... 
+  // ^ The address of the FavoriteNumber contract on Ethereum
+  NumberInterface numberContract = NumberInterface(NumberInterfaceAddress);
+  // Now `numberContract` is pointing to the other contract
+
+  function someFunction() public {
+    // Now we can call `getNum` from that contract:
+    uint num = numberContract.getNum(msg.sender);
+    // ...and do something with `num` here
+  }
+  
+}
+
+>In this way, your contract can interact with any other contract on the Ethereum blockchain, as long they expose those functions as public or external.
