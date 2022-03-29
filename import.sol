c06f7a7d3d56a416f6a6873bbcd42fba7f1ff866
gcr07 <63270579+gcr07@users.noreply.github.com>
2022-03-29 10:11:18 -0600

    Create import.sol

---
+++ import.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+// put import statement here
+
+import "./zombiefactory.sol"; // Se usa para importar modulos si te das cuenta usa herencia y asi se manejan proyectos largos.
+
+contract ZombieFeeding is ZombieFactory {
+
+}
