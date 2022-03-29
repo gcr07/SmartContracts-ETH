ba5e9510666f27fa0af8b5c455a8e26c9a285472
gcr07 <63270579+gcr07@users.noreply.github.com>
2022-03-29 11:05:12 -0600

    Update Interacting with other contracts.sol

--- Interacting with other contracts.sol
+++ Interacting with other contracts.sol
@@ -1,1 +1,19 @@
 
+//Interacting with other contracts se tiene que crear una interfaz y dentro la pura funcion ejemplo:
+
+contract LuckyNumber {
+  mapping(address => uint) numbers;
+
+  function setNum(uint _num) public {
+    numbers[msg.sender] = _num;
+  }
+
+  function getNum(address _myAddress) public view returns (uint) {
+    return numbers[_myAddress];
+  }
+}
+
+
+contract NumberInterface {
+  function getNum(address _myAddress) public view returns (uint);// no tiene llaves ni nada
+}
