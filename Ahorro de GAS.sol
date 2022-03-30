ab6dc28e27e0a9b4f77dd4163c0f5a12f74773ec
gcr07 <63270579+gcr07@users.noreply.github.com>
2022-03-29 21:28:42 -0600

    Create Ahorro de GAS.sol

---
+++ Ahorro de GAS.sol
@@ -0,0 +1,15 @@
+struct NormalStruct {
+  uint a;
+  uint b;
+  uint c;
+}
+
+struct MiniMe {
+  uint32 a;
+  uint32 b;
+  uint c;
+}
+
+// `mini` will cost less gas than `normal` because of struct packing
+NormalStruct normal = NormalStruct(10, 20, 30);
+MiniMe mini = MiniMe(10, 20, 30); 
