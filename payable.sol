4eb559788b33fe3e76b9c8024011bcd4bc195f91
gcr07 <63270579+gcr07@users.noreply.github.com>
2022-03-31 09:47:08 -0600

    Create payable.sol

---
+++ payable.sol
@@ -0,0 +1,13 @@
+contract OnlineStore {
+  function buySomething() external payable {
+    // Check to make sure 0.001 ether was sent to the function call:
+    require(msg.value == 0.001 ether);
+    // If so, some logic to transfer the digital item to the caller of the function:
+    transferThing(msg.sender);
+  }
+}
+
+Here, msg.value is a way to see how much Ether was sent to the contract, and ether is a built-in unit.
+
+
+
